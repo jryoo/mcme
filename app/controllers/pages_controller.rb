6f9ab5e06c724cd6194789100b42aaba32c6b63b
Jae Young Ryoo <jay.ryoo@gmail.com>
2013-05-28 02:17:53 -0700

    random posts

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,8 +8,9 @@ class PagesController < ApplicationController
 																:consumer_secret => ENV['CONSUMER_SECRET'], 
 																:oauth_token => ENV['ACCESS_TOKEN'], 
 																:oauth_token_secret => ENV['ACCESS_TOKEN_SECRET'])
-		@text = client.posts("bigbluemcme.tumblr.com")
-
+		index = Random.new.rand(20)
+		posts = client.posts("bigbluemcme.tumblr.com/", :type => "photo")
+		@url = posts["posts"][index]["photos"][0]["original_size"]["url"]
 
   end
 
