b827893e3899028f1389fb60721c10f6e1673aa0
Jae Young Ryoo <jay.ryoo@gmail.com>
2013-05-28 01:54:10 -0700

    work

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,7 +8,7 @@ class PagesController < ApplicationController
 																:consumer_secret => ENV['CONSUMER_SECRET'], 
 																:oauth_token => ENV['ACCESS_TOKEN'], 
 																:oauth_token_secret => ENV['ACCESS_TOKEN_SECRET'])
-		@text = client.posts("http://bigbluemcme.tumblr.com/")
+		@text = client.posts("bigbluemcme.tumblr.com")
 
 
   end
